d2187b148a73c50952c67993721c4a82495ca36c
bmasscha <45875018+bmasscha@users.noreply.github.com>
2026-02-14 12:26:51 +0100

    Cleanup: Remove obsolete build files and old GUI scripts

--- build_exe.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo [*] Installing dependencies...
-pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org --default-timeout=100 -r requirements.txt
-echo [*] Building executable...
-pyinstaller --noconsole --onefile --name "RSS_Audio_Grabber" --collect-all PyQt6 --clean rss_to_mp3_gui.py
-echo [*] Build complete! Check the 'dist' folder.
-pause
